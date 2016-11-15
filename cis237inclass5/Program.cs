fbba54ada471f0ba41500ae150fb2b03220f6d36
Robert Cooely <Daimyo121@aol.com>
2016-11-15 17:14:50 -0500

    Add and Delete with database.

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -79,7 +79,115 @@ namespace cis237inclass5
             {
                 Console.WriteLine(car.id + " " + car.make + " " + car.model);
             }
-            
+            //*************************
+            //Add a new car to the database
+            //*************************
+
+            //Make an instance of a new car
+            Car newCarToAdd = new Car();
+            //Assign properties to the parts of the model.
+            newCarToAdd.id = "88888";
+            newCarToAdd.make = "Nissan";
+            newCarToAdd.model = "GT-R";
+            newCarToAdd.horsepower = 550;
+            newCarToAdd.cylinders = 8;
+            newCarToAdd.year = "2016";
+            newCarToAdd.type = "Car";
+
+            try
+            {
+                //Add the new car to the Cars Collection
+                carsRCooleyEnties.Cars.Add(newCarToAdd);
+
+                //Persist the collection to the database.
+                //This call will actually do the work of saving the changes to the database.
+                carsRCooleyEnties.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                //Remove the new car from the Cars Collection since we can't save it.
+                carsRCooleyEnties.Cars.Remove(newCarToAdd);
+
+                //This catch might get thrown for other reasons than a primary key error.
+                //Here I am assuming that.
+                Console.WriteLine("Can't add the record. Already have one with that primary key");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Just added a new car. Going to fetch and print to verify");
+            carToFind = carsRCooleyEnties.Cars.Find("88888");
+            Console.WriteLine(carToFind.id+ " "+ carToFind.make+" "+ carToFind.model+" ");
+
+            //***********************
+            //Update a record
+            //***********************
+
+            //Get out the car we want to update
+            Car carToFindForUpdate = carsRCooleyEnties.Cars.Find("88888");
+
+            //Output the car to find before the update.
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("About to do an update on the following car");
+            Console.WriteLine(carToFindForUpdate.id + " " + carToFindForUpdate.make + " " + carToFindForUpdate.model + " ");
+
+            Console.WriteLine("Doing the update now?");
+
+            //Update some of the properties of the new car we found. We don't have to update all of the fields if we don't want to.
+            carToFindForUpdate.make = "Nissssssan";
+            carToFindForUpdate.model = "Gt-ARRRRRRRRGGGGGGGGGHHHH";
+            carToFindForUpdate.horsepower = 1845;
+            carToFindForUpdate.cylinders = 16;
+
+            //Save the changes to the database. Since when we pulled out the one to update
+            //all we were really doing was getting a reference to the one in the collection we wanted to
+            //update. There is no need to 'put' the car back into the Cars collection.
+            //It is still there. All we have to do is save the changes.
+
+            carsRCooleyEnties.SaveChanges();
+
+            //Get the car out now that it has been updated to verify the update worked.
+            carToFindForUpdate = carsRCooleyEnties.Cars.Find("88888");
+            Console.WriteLine(carToFindForUpdate.id + " " + carToFindForUpdate.make + " " + carToFindForUpdate.model + " ");
+
+            //OUtput the updated Car
+            Console.WriteLine("Outputing the updated car");
+
+            //***********************
+            //How to Delete a record
+            //***********************
+
+            //Get a car out of the database that we would like to delete.
+            Car carToFindForDelete = carsRCooleyEnties.Cars.Find("88888");
+
+            //Remove the car
+            carsRCooleyEnties.Cars.Remove(carToFindForDelete);
+
+            //Save the changes to the database.
+            carsRCooleyEnties.SaveChanges();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Deleted the added car. Looking to see if it is still in the DB");
+
+            //Check to see if the car was deleted
+            try
+            {
+                //This statement will execute just fine. It's when we go to access the property
+                //id on null that the exception will get thrown.
+                carToFindForDelete = carsRCooleyEnties.Cars.Find("88888");
+                Console.WriteLine(carToFindForDelete.id + " " + carToFind.make + " " + carToFind.model + " ");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The model you are looking for does not exist" + e.ToString()+ " "+ e.StackTrace);
+            }
+
+            //Another way to check to see if the record has been deleted
+            if(carToFindForDelete==null)
+            {
+                Console.WriteLine("The model you are looking for does not exist");
+            }
         }
     }
 }
